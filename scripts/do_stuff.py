ff300a57c0873b2d575eab4d572eeb115f3c9db2
Florin Suciu <florin.suciu@ntti3.io>
2019-06-19 13:36:20 +0300

    Trying to reproduce.

--- scripts/do_stuff.py
+++ scripts/do_stuff.py
@@ -1,19 +1,31 @@
 import os
+import threading
 import time
 
+THREAD_COUNT = 3
 COUNT_TO = 25
 SLEEP_TIME = 1
 
 
-def count(nr):
+def count(nr, raise_exception):
     for i in range(nr):
         time.sleep(SLEEP_TIME)
         print(i)
+    if raise_exception:
+        raise Exception('Something went wrong.')
 
 
 if __name__ == '__main__':
     print('Counting to {}.'.format(COUNT_TO))
-    count(COUNT_TO)
+    raise_exc = False
+    threads = []
+    for index in range(THREAD_COUNT):
+        if index == 1:
+            raise_exc = True
+        t = threading.Thread(target=count, args=(COUNT_TO, raise_exc))
+        threads.append(t)
+        t.start()
+    for t in threads:
+        t.join()
     print('Secret is: {}.'.format(os.getenv('SECRET')))
     print('Done counting.')
-    raise Exception('something')
