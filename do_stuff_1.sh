d1aa97c6a26743714bef923ca276519abbcf94c0
Florin Suciu <florin.suciu@ntti3.io>
2019-06-19 12:51:55 +0300

    Added shell scripts to try and reproduce issue.

---
+++ do_stuff_1.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+echo "Doing stuff 1 start."
+sleep 7
+echo "Doing stuff 1 mid."
+sleep 7
+echo "Doing stuff 1 end."
---
+++ do_stuff_2.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+echo "Doing stuff 2 start."
+sleep 10
+echo "Doing stuff 2 mid."
+sleep 10
+echo "Doing stuff 2 end."
